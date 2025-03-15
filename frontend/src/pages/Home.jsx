2504694909377476941aff3684109a6473bbd8c4
Nischal <tamangnischal2018@gmail.com>
2025-03-15 20:39:52 +0545

    working on hero section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <>
+      <section className="hero_section pt-[60px] 2xl:h-[800px]">
+        <div className="container">
+          <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
+            {/* content starts */}
+            <div>
+              <div className="lg:w-[570px]">
+                <h1 className="text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:leading-[70px]">
+                  We help patients live a healthy, longer life.
+                </h1>
 
-export default Home
+                <p className="text_para">
+                Our platform connects patients with trusted doctors, making appointments easy and accessible. Prioritize your health with seamless scheduling and expert medical care.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
