100511c35193cf5094eca134cd00a9c86118dfd6
Nischal <tamangnischal2018@gmail.com>
2025-03-17 08:56:02 +0545

    working on feature section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,6 +10,8 @@ import { Link } from "react-router-dom";
 import { BsArrowRight } from "react-icons/bs";
 import About from "../components/About/About";
 import ServiceList from "../components/Services/ServiceList";
+import featureImg from "../assets/images/feature-img.png";
+import videoIcon from "../assets/images/video-icon.png";
 
 const Home = () => {
   return (
@@ -205,6 +207,31 @@ const Home = () => {
               <Link to="/">
                 <button className="btn">Learn More</button>
               </Link>
+            </div>
+
+            <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
+              <img src={featureImg} alt="feature image" className="w-3/4" />
+              <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-[6px] lg:gap-3">
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]">
+                      Mon, 24
+                    </p>
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-textColor font-[400]">
+                      10:00 AM
+                    </p>
+                  </div>
+                  <span className="w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px">
+                    <img src={videoIcon} alt="" />
+                  </span>
+                </div>
+                <div className="w-[65px] lg:w-[96px] bg-[#ccf0f3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4 rounded-full ">
+                  {" "}
+                  Consultation{" "}
+                </div>
+
+                
+              </div>
             </div>
           </div>
         </div>
