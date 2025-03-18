ec9e1141e9130d37e436889efdb81baa0e5b414c
Nischal <tamangnischal2018@gmail.com>
2025-03-18 13:30:22 +0545

    workgin on footer section

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,4 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import logo from '../../assets/images/logo.png'
+import {RiLinkedinFill} from 'react-icons/ri'
+import {AiFillYoutube, AiFillGithub, AiOutlineInstagram} from 'react-icons/ai'
+
+
+const socialLinks = [
+  {
+    path: "https://www.youtube.com/@Nischaltamang2023",
+    icon: <AiFillYoutube className='group-hover:text-white w-4 h-5'/>,
+  },
+  {
+path: ""
+  }
+]
 
 const Footer = () => {
   return (
