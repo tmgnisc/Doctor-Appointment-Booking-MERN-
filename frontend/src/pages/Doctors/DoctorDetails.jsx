0bc4f7c73f25ff310a03cf7432930d36ee3e9782
Nischal <tamangnischal2018@gmail.com>
2025-03-20 17:24:02 +0545

    made about feedback button tab toggle

--- frontend/src/pages/Doctors/DoctorDetails.jsx
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import doctorImg from "../../assets/images/doctor-img02.png";
-import starIcon from "../../assets/images/Star.png"
+import starIcon from "../../assets/images/Star.png";
 
 const DoctorDetails = () => {
+  const [tab, setTab] = useState("about");
   return (
     <section>
       <div className="max-w-[1170px] px-5 mx-auto">
         <div className="grid md:grid-cols-3 gap-[50px]">
-          <div className="md:col-span-2">    
+          <div className="md:col-span-2">
             <div className="flex items-center gap-5">
               <figure className="max-w-[200px] max-h-[200px]">
                 <img src={doctorImg} alt="" className="w-full" />
@@ -22,21 +23,39 @@ const DoctorDetails = () => {
                 </h3>
                 <div className="flex items-center gap-[6px]">
                   <span className="flex items-center gap-[6px] text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-semibold text-headingColor">
-                    <img src={starIcon} alt="" />4.8
+                    <img src={starIcon} alt="" />
+                    4.8
                   </span>
-                  <span className="text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-[400] text-textColor">(272)</span>
+                  <span className="text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-[400] text-textColor">
+                    (272)
+                  </span>
                 </div>
                 <p className="text_para text-[14px] leading-6 md:text-[15px] lg:max-w-[390px]">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore, qui!
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Inventore, qui!
                 </p>
               </div>
             </div>
 
-<div className="mt-[50px] border-b border-solid border-[#0066ff34]">
-<button className={`pyt-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}> About</button>
-</div>
-
-
+            <div className="mt-[50px] border-b border-solid border-[#0066ff34]">
+              <button
+                onClick={() => setTab("about")}
+                className={` ${
+                  tab === "about" && "border-b border-solid border-primaryColor"
+                }  pyt-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}
+              >
+                About
+              </button>
+              <button
+                onClick={() => setTab("feedback")}
+                className={`${
+                  tab === "feedback" &&
+                  "border-b border-solid border-primaryColor"
+                } pyt-2 px-5 mr-5 text-[16px] leading-7 text-headingColor font-semibold`}
+              >
+                Feedback
+              </button>
+            </div>
           </div>
           <div></div>
         </div>
