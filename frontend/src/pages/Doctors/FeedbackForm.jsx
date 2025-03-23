4b4d7e1a319a69efb6d460cf2b7aaa96eea3ea5b
Nischal <tamangnischal2018@gmail.com>
2025-03-23 10:35:02 +0545

    working on start review

--- frontend/src/pages/Doctors/FeedbackForm.jsx
+++ frontend/src/pages/Doctors/FeedbackForm.jsx
@@ -1,23 +1,44 @@
-import React from 'react'
-import {AiFillStar} from "react-icons/ai"
+import React, { useState } from "react";
+import { AiFillStar } from "react-icons/ai";
 
 const FeedbackForm = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
   return (
     <div>
-    <h3 className='text-headingColor text-[16px] leading-6 font-semibold mb-4'>How would you rate the overall experience?*</h3>
+      <h3 className="text-headingColor text-[16px] leading-6 font-semibold mb-4">
+        How would you rate the overall experience?*
+      </h3>
 
-    <div>
-      {[...Array(5).keys()].map((_,index)=>{
-        index+=1
-        return <button key={index} type='button'>
-          <span>
-            <AiFillStar/>
-          </span>
-        </button>
-      })}
+      <div>
+        {[...Array(5).keys()].map((_, index) => {
+          index += 1;
+          return (
+            <button
+              key={index}
+              className={`${
+                index <= ((rating && hover) || hover)
+                  ? "text-yellowColor"
+                  : "text-gray-400"
+              } bg-transparent border-none outline-none text-[22px] cursor-pointer`}
+              onClick={() => setRating(index)}
+              onMouseEnter={() => setHover(index)}
+              onMouseLeave={() => setHover(rating)}
+              onDoubleClick={() => {
+                setHover(0);
+                setRating(0);
+              }}
+              type="button"
+            >
+              <span>
+                <AiFillStar />
+              </span>
+            </button>
+          );
+        })}
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default FeedbackForm
+export default FeedbackForm;
