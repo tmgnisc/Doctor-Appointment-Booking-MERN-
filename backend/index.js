bba8624ae65be9070714b4486c0842bba6b545bb
Nischal <tamangnischal2018@gmail.com>
2025-03-25 16:05:38 +0545

    working on backend port 500

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from 'cors'
+import mongoose from "mongoose"
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 8000
+
+const corsOptions = {
+    origin:true 
+}
+
+app.get('/', (req, res)=>{
+    res.send('Api is working')
+})
+app.listen(port, ()=>{
+    console.log("server is running on port" + port)
+})
