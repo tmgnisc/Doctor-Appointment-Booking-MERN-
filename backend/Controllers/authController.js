fbceaa93cd4c4383fe3c015c95304dc0237f5971
Nischal <tamangnischal2018@gmail.com>
2025-03-26 12:38:32 +0545

    testing api for register

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -6,17 +6,50 @@ export const register = async (req, res) => {
   const { email, password, name, role, photo, gender } = req.body;
   try {
     let user = null;
-    if(role === 'patient'){
-        user = User.findOne({email})
-    } else if(role === 'doctor'){
-        user = Doctor.findOne({email})
+    if (role === "patient") {
+      user = await User.findOne({ email });
+    } else if (role === "doctor") {
+      user = await Doctor.findOne({ email });
     }
 
-
     //check if user exist
-    
-  } catch (err) {
+    if (user) {
+      return res.status(400).json({ message: "user already exists" });
+    }
 
+    //hash password
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    if (role === "patient") {
+      user = new User({
+        name,
+        email,
+        password: hashPassword,
+        photo,
+        gender,
+        role,
+      });
+
+      if (role === "doctor") {
+        user = new Doctor({
+          name,
+          email,
+          password: hashPassword,
+          photo,
+          gender,
+          role,
+        });
+      }
+    }
+
+    await user.save();
+    res
+      .status(200)
+      .json({ success: true, message: "User successfully created" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
 
