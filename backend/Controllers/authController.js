7ed0d5b3807d2b5e7984c15e037afae68a183820
Nischal <tamangnischal2018@gmail.com>
2025-03-26 13:01:49 +0545

    login register backend api working

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -3,10 +3,11 @@ import Doctor from "../models/DoctorSchema.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
-
-const generateToken = user =>{
-    return jwt.sign({id:user._id, role:user.role}, process.env.JWT_SECRET)
-}
+const generateToken = (user) => {
+  return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "30d",
+  });
+};
 export const register = async (req, res) => {
   const { email, password, name, role, photo, gender } = req.body;
   try {
@@ -59,28 +60,42 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-
-    const {email, password} = req.body
+  const { email } = req.body;
   try {
-
-    let user = null
-    const patient = await User.findOne({email})
-    const doctor = await Doctor.findOne({email})
-    if(patient){
-        user = patient
-    } if(doctor){
-        user = doctor
+    let user = null;
+    const patient = await User.findOne({ email });
+    const doctor = await Doctor.findOne({ email });
+    if (patient) {
+      user = patient;
+    }
+    if (doctor) {
+      user = doctor;
     }
 
-    if(!user){
-        return res.status(404).json({message:"user not found"})
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
     }
 
     //compare password
 
-    const isPasswordMatch = await bcrypt.compare(password, user.password)
-    if(!isPasswordMatch){
-        return res.status(400).json({message:"invalid credentials"})
+    const isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(400).json({ message: "invalid credentials" });
     }
-  } catch (err) {}
+    //get token
+    const token = generateToken(user);
+    const { password, role, appointments, ...rest } = user._doc;
+
+    res
+      .status(200)
+      .json({
+        status: true,
+        message: "successfully login",
+        token,
+        data: { ...rest },
+        role,
+      });
+  } catch (err) {
+    res.status(500).json({ status: false, message: "Invalid login" });
+  }
 };
