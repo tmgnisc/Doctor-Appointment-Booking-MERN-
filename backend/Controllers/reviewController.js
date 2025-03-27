6ea5cc421be54a1a8a8ae630397c39fda48c30e5
Nischal <tamangnischal2018@gmail.com>
2025-03-27 12:13:44 +0545

    completed review controller js

--- backend/Controllers/reviewController.js
+++ backend/Controllers/reviewController.js
@@ -13,3 +13,21 @@ export const getAllReviews = async (req, res) =>{
     }
 }
 
+//create reviews
+
+export const createReview = async(req, res)=>{
+    if(!req.body.doctor) req.body.doctor = req.params.doctorId 
+    if(!req.body.user) req.body.user = req.userId 
+    const savedReview = new Review(req.body)
+    try{
+        const savedReview = await newReview.save()
+        await Doctor.findByIdAndUpdate(req.body.doctor, {
+            $push:{reviews:savedReview._id}
+        })
+
+        res.status(200).json({success:true, message:'Review Submitted'})
+    } catch(err){
+        res.status(500).json({success:false, message:err.message})
+    }
+
+}
