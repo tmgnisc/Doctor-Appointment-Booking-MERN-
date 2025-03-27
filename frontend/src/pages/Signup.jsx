04b41a4067936b06a5edd5192d55d0139c7be89e
Nischal <tamangnischal2018@gmail.com>
2025-03-27 19:10:48 +0545

    register api included success

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,11 +1,16 @@
 import signupImg from "../assets/images/signup.gif";
 import avatar from "../assets/images/doctor-img01.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import uploadImageToCloudinary from "../../utils/uploadCloudinary.js";
+import { BASE_URL } from "../../config.js";
+import { toast } from "react-toastify";
+import HashLoader from "react-spinners/HashLoader";
 
 const Signup = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [previewURL, setPreviewURL] = useState("");
+  const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -15,6 +20,7 @@ const Signup = () => {
     role: "patient",
   });
 
+  const navigate = useNavigate();
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
@@ -22,15 +28,38 @@ const Signup = () => {
   const handleFileInputChange = async (event) => {
     const file = event.target.files[0];
 
-    //later use cloudinary to store and upload images 
+    const data = await uploadImageToCloudinary(file);
+    console.log(data);
+    setPreviewURL(data.url);
+    setSelectedFile(data.url);
+    setFormData({ ...formData, photo: data.url });
+    //later use cloudinary to store and upload images
     console.log(file);
   };
 
-  const submitHandler = async event => {
-
-    console.log(formData)
-    e.preventDefault()
-  }
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    try {
+      const res = await fetch(`${BASE_URL}/auth/register`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const { message } = await res.json();
+      if (!res.ok) {
+        throw new Error(message);
+      }
+      setLoading(false);
+      toast.success(message);
+      navigate("/login");
+    } catch (error) {
+      toast.error(err.message);
+      setLoading(false);
+    }
+  };
   return (
     <section className="px-5 xl:px-0">
       <div className="max-w-[1170px] mx-auto">
@@ -113,9 +142,15 @@ const Signup = () => {
               </div>
 
               <div className="mb-5 flex items-center gap-3">
-                <figure className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center">
-                  <img src={avatar} alt="" className="w-full rounded-full" />
-                </figure>
+                {selectedFile && (
+                  <figure className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center">
+                    <img
+                      src={previewURL}
+                      alt=""
+                      className="w-full rounded-full"
+                    />
+                  </figure>
+                )}
 
                 <div className="relative w-[130px] h-[50px]">
                   <input
@@ -138,10 +173,15 @@ const Signup = () => {
 
               <div className="mt-7">
                 <button
+                  disabled={loading && true}
                   type="submit"
                   className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg py-3"
                 >
-                  Sign Up
+                  {loading ? (
+                    <HashLoader size={35} color="#ffffff" />
+                  ) : (
+                    "Sign Up"
+                  )}
                 </button>
               </div>
 
--- frontend/utils/uploadCloudinary.js
+++ frontend/utils/uploadCloudinary.js
@@ -5,6 +5,17 @@ const uploadImageToCloudinary = async (file) => {
   uploadData.append("file", file);
   uploadData.append("upload_preset", upload_preset);
   uploadData.append("cloud_name", cloud_name);
+
+  const res = await fetch(
+    `https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`,
+    {
+      method: "post",
+      body: uploadData,
+    }
+  );
+
+  const data = await res.json();
+  return data;
 };
 
 export default uploadImageToCloudinary;
