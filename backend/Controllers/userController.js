c17bf4901742d2b5b817a429457446f2394a3a8c
Nischal <tamangnischal2018@gmail.com>
2025-03-27 21:13:10 +0545

    working on controller for user profile

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -59,3 +59,33 @@ export const getAllUser = async (req, res) => {
       res.status(404).json({ success: false, message: "not found" });
     }
   };
+
+
+  export const getUserProfile = async (req, res) => {
+    const userId = req.userId;
+  
+    try {
+      const user = await User.findById(userId);
+  
+      if (!user) {
+        return res
+          .status(404)
+          .json({ success: false, message: "User not found" });
+      }
+  
+      const { password, ...rest } = user._doc;
+  
+      res
+        .status(200)
+        .json({
+          success: true,
+          message: "Profile info is getting",
+          data: { ...rest },
+        });
+    } catch (err) {
+      res
+        .status(500)
+        .json({ success: false, message: "Something went wrong, cannot get" });
+    }
+  };
+  
