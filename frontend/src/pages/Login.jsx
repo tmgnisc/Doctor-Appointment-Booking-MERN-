d4f8f5dc0ab75b887a2a0410d2a6d32c6b2c95a4
Nischal <tamangnischal2018@gmail.com>
2025-03-27 21:06:47 +0545

    working on login and navigation header

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,6 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { BASE_URL } from "../../config";
 import { toast } from "react-toastify";
 import { authContext } from "../context/AuthContext.jsx";
+import HashLoader from "react-spinners/HashLoader";
+
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -90,7 +92,7 @@ const Login = () => {
               type="submit"
               className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg py-3"
             >
-              Login
+              {loading ? <HashLoader size={25} color="#ffffff"/> : 'Login'}
             </button>
           </div>
 
