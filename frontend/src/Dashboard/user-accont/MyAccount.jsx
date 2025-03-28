03260958b97bbdc56d822e3a0ac6b2038d00fc32
Nischal <tamangnischal2018@gmail.com>
2025-03-28 10:28:25 +0545

    completed user profile update

--- frontend/src/Dashboard/user-accont/MyAccount.jsx
+++ frontend/src/Dashboard/user-accont/MyAccount.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import userImg from "../../assets/images/doctor-img01.png";
+
 import { authContext } from "../../context/AuthContext";
 import MyBookings from "./MyBookings";
 import Profile from "./Profile";
@@ -35,7 +35,7 @@ const MyAccount = () => {
               <div className="flex items-center justify-center">
                 <figure className="w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor">
                   <img
-                    src={userImg}
+                    src={userData.photo}
                     alt=""
                     className="w-full h-full rounded-full"
                   />
@@ -44,15 +44,15 @@ const MyAccount = () => {
 
               <div className="text-center mt-4">
                 <h3 className="text-[18px] leading-[30px] text-headingColor font-bold ">
-                  Muhibur Rahman
+                  {userData.name}
                 </h3>
                 <p className="text-textColor text-[15px] leading-6 font-medium ">
-                  example@gmail.com
+                 {userData.email}
                 </p>
                 <p className="text-textColor text-[15px] leading-6 font-medium ">
                   Blood Type:
                   <span className="ml-2 text-headingColor text-[22px] leading-8">
-                    O-
+                   {userData.bloodType}
                   </span>
                 </p>
               </div>
@@ -94,7 +94,7 @@ const MyAccount = () => {
               </div>
 
               {tab === "bookings" && <MyBookings />}
-              {tab === "settings" && <Profile />}
+              {tab === "settings" && <Profile user={userData} />}
             </div>
           </div>
         )}
