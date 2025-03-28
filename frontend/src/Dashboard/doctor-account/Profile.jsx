4e046a6f8144796cda4d79273717cbb2dc9f1137
Nischal <tamangnischal2018@gmail.com>
2025-03-28 11:52:32 +0545

    working on doctor profile

---
+++ frontend/src/Dashboard/doctor-account/Profile.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+const Profile = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone:"",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div>
+      <h2 className="text-headingColor font-bold text-[24px] leading-9 mb-10">
+        Profile Information
+      </h2>
+
+      <form>
+        <div className="mb-5">
+          <p className="form__label">Name</p>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            placeholder="Full Name"
+            className="form_input"
+          />
+        </div>
+
+        <div className="mb-5">
+          <p className="form__label">Email</p>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            placeholder="Email"
+            className="form_input"
+            readOnly
+            aria-readonly
+            disabled={true}
+          />
+        </div>
+
+        <div className="mb-5">
+          <p className="form__label">Phone</p>
+          <input
+            type="number"
+            name="phone"
+            value={formData.phone}
+            onChange={handleInputChange}
+            placeholder="Phone Number"
+            className="form_input"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Profile;
